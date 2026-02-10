0765cc7c762082d6e3f7d4453246aa389d6d0b62
jojotjo <144655367+jojotjo@users.noreply.github.com>
2026-02-10 12:33:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -162,4 +162,6 @@ app.post("/bfhl", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Running on ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
